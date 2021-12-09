9cd71615fb98a97993b57cebc74463166d0f90b4
dennisshtavica <dennisshtavica@gmail.com>
2021-12-10 00:07:53 +0100

    update

--- JS/main.js
+++ JS/main.js
@@ -18,6 +18,24 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 
 // Swiper
 
