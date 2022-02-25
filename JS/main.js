f59687fa96f09dea81cc47892ef4d0ba95d22c33
dennisshtavica <dennisshtavica@gmail.com>
2022-02-25 21:50:52 +0100

    added validation

--- JS/main.js
+++ JS/main.js
@@ -19,44 +19,44 @@ if (navClose) {
   });
 }
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
-
+window.addEventListener("scroll", scrollHeader);
 
 // SWIPER
 
-  let swiper = new Swiper(".destinations-swiper", {
-    // cssMode: true,
-    // centeredSlides: true,
-    // slidesPerView: '3',
-    // spaceBetween: -90,
-    loop: 'true',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    // mousewheel: true,
-    keyboard: true,
-  });
+let swiper = new Swiper(".destinations-swiper", {
+  // cssMode: true,
+  // centeredSlides: true,
+  // slidesPerView: '3',
+  // spaceBetween: -90,
+  loop: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // mousewheel: true,
+  keyboard: true,
+});
 
-  /*==================== SCROLL REVEAL ANIMATION ====================*/
+/*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
   // distance: '60px',
   // duration: 2800,
@@ -67,7 +67,7 @@ const sr = ScrollReveal({
   delay: 100,
 });
 
-sr.reveal('.nav');
+sr.reveal(".nav");
 sr.reveal(".home__container");
 
 const sr2 = ScrollReveal({
@@ -77,18 +77,18 @@ const sr2 = ScrollReveal({
   delay: 100,
   reset: true,
 });
-sr2.reveal('.cards');
-sr2.reveal('.grid__cards');
+sr2.reveal(".cards");
+sr2.reveal(".grid__cards");
 
-const sr3 = ScrollReveal({ 
+const sr3 = ScrollReveal({
   reset: true,
   origin: "left",
   distance: "60px",
   duration: 1500,
   delay: 100,
-})
+});
 
-sr3.reveal('.condition1');
+sr3.reveal(".condition1");
 
 const sr4 = ScrollReveal({
   reset: true,
@@ -102,42 +102,48 @@ sr4.reveal(".condition2");
 
 // VALIDATE
 
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
+function formValidation(formId, inputId, pattern) {
+  const value = document.getElementById(inputId).value;
+  const form = document.getElementById(formId);
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  checkInputs();
-});
-
-function checkInputs(){
-  const userValue = username.value.trim();
-  const passValue = password.value.trim();
-
-  if(userValue === ''){
-    setErrorFor(username)
-  }
-  else {
-      setSuccessFor(username);
+  if (value.match(pattern)) {
+    form.classList.add("success");
+    form.classList.remove("error");
+    return true;
+  } else {
+    form.classList.remove("success");
+    form.classList.add("error");
+    return false;
   }
 }
 
-function setError(input){
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
+function emailValidation() {
+  const emailPattern =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  formValidation("email-form", "email", emailPattern);
 }
 
-
-function setSuccessFor(input){
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success';
+function passwordValidation() {
+  const passPattern = /.{6}/;
+  formValidation("pass-form", "pass", passPattern);
+  confirmPassValidation();
 }
 
+function userValidation() {
+  const userPattern = /.{6}/;
+  formValidation("user-form", "user", userPattern);
+}
 
-
-
-
-
-
+function confirmPassValidation() {
+  const pass = document.getElementById("pass").value;
+  const confirmPass = document.getElementById("confirm-pass").value;
+  const form = document.getElementById("confirm-pass-form");
+  const pform = document.getElementById("pass-form");
+  if (pass != "" && pass == confirmPass && pform.classList.contains("success")) {
+    form.classList.add("success");
+    form.classList.remove("error");
+  } else {
+    form.classList.remove("success");
+    form.classList.add("error");
+  }
+}
--- views/register.php
+++ views/register.php
@@ -1,84 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Sign In</title>
     <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css" />
     <link rel="stylesheet" type="text/css" href="/CSS/style.css" />
     <link rel="stylesheet" type="text/css" href="CSS/loginStyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-     <link rel="shortcut icon" href="/assets/images/favicon-32x32.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/assets/images/favicon-32x32.png" type="image/x-icon">
     <!-- Boxicons -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
     <link rel="stylesheet" href="./CSS/fontello.css" />
     <link rel="stylesheet" href="./CSS/loginStyle.css" />
-  </head>
+</head>
 
-  <body>
+<body>
     <!-- HEADER -->
     <header class="header" id="header">
-      <nav class="nav container">
-        <a href="indexasdfas.html" class="nav__logo"> Adventure </a>
-        <div class="nav__menu" id="nav-menu">
-          <ul class="nav__list">
-            <li class="nav__item">
-              <a href="indexasdfas.html" class="nav__link">Home</a>
-            </li>
-            <li class="nav__item">
-              <a href="#" class="nav__link">Activities</a>
-            </li>
-            <li class="nav__item">
-              <a href="#" class="nav__link nav__about">About Us</a>
-            </li>
-            <li class="nav__item">
-              <a href="register.php" class="nav__link nav__register"
-                >Sign In</a
-              >
-            </li>
-          </ul>
+        <nav class="nav container">
+            <a href="indexasdfas.html" class="nav__logo"> Adventure </a>
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item">
+                        <a href="indexasdfas.html" class="nav__link">Home</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#" class="nav__link">Activities</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#" class="nav__link nav__about">About Us</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="register.php" class="nav__link nav__register">Sign In</a>
+                    </li>
+                </ul>
 
-          <div class="nav__close" id="nav-close">
-            <i class="bx bx-x"></i>
-          </div>
-        </div>
-        <div class="nav__toggle" id="nav-toggle">
-          <img src="/assets/images/burger-bar.png" alt="" />
-          <!-- <i class='bx bx-menu'></i> -->
-        </div>
-      </nav>
+                <div class="nav__close" id="nav-close">
+                    <i class="bx bx-x"></i>
+                </div>
+            </div>
+            <div class="nav__toggle" id="nav-toggle">
+                <img src="/assets/images/burger-bar.png" alt="" />
+                <!-- <i class='bx bx-menu'></i> -->
+            </div>
+        </nav>
     </header>
     <div class="center">
-<!--      <img class="login-bg" src="assets/images/test1.webp">-->
-      <div class="card" id="card">
-        <form action="#" class="front">
-          <h1>Sign in</h1>
-          <div class="group"><i class="icon-mail-alt"></i><input class="email" type="email" placeholder="Email"></div>
-          <div class="group"><i class="icon-lock"></i><input class="password" type="password" placeholder="Password"></div>
-          <input type="submit" id="btn" value="Login">
+        <!--      <img class="login-bg" src="assets/images/test1.webp">-->
+        <div class="card" id="card">
+            <form action="#" class="front">
+                <h1>Sign in</h1>
+                <div class="group"><i class="icon-mail-alt">
+                    </i><input class="email" type="email" placeholder="Email" required>
+                </div>
+                <div class="group"><i class="icon-lock"></i>
+                    <input class="password" type="password" placeholder="Password" required>
+                </div>
+                <input type="submit" id="btn" value="Login">
 
-          <h2 id="register">Register</h2>
-        </form>
+                <h2 id="register">Register</h2>
+            </form>
 
-        <form action="/register" method="POST" class="back">
-          <h1>Register</h1>
-          <div class="group"><i class="icon-user"></i><input class="name" name="name" type="text" placeholder="Name"></div>
-          <div class="group"><i class="icon-mail-alt"></i><input class="email" name="email" type="email" placeholder="Email"></div>
-          <div class="group"><i class="icon-lock"></i><input class="password" name="password" type="password" placeholder="Password"></div>
-          <div class="group"><i class="icon-lock"></i><input class="password" name="confirm_password" type="password" placeholder="Confirm password"></div>
-          <input type="submit" id="btnR" value="Register">
+            <form action="/register" method="POST" class="back">
+                <h1>Register</h1>
+                <div class="group"><i class="icon-user"></i>
+                    <input class="name" name="name" type="text" placeholder="Name" name="username" required>
+                </div>
+                <div class=" group"><i class="icon-mail-alt"></i>
+                    <input class="email" name="email" type="email" placeholder="Email" name="email" required>
+                </div>
+                <div class="group"><i class="icon-lock"></i>
+                    <input class="password" name="password" type="password" placeholder="Password" name="password"
+                        required>
+                </div>
+                <div class="group"><i class="icon-lock"></i>
+                    <input class="password" name="confirm_password" type="password" placeholder="Confirm password"
+                        name="cpassword" required>
+                </div>
+                <input type="submit" id="btnR" value="Register">
 
-          <h2 id="Login">Login</h2>
-        </form>
+                <h2 id="Login">Login</h2>
+            </form>
 
-      </div>
+        </div>
     </div>
 
 
     <script type="text/javascript" src="event.js"></script>
- </div>
+    </div>
     <script src="/JS/event.js"></script>
     <script src="/JS/main.js"></script>
-  </body>
+</body>
+
 </html>
