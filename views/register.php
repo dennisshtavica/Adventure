b9c655a3a24cfb5338d2c578eb8897bd797cfb49
dennisshtavica <dennisshtavica@gmail.com>
2022-02-26 17:32:28 +0100

    edit

--- views/register.php
+++ views/register.php
@@ -5,9 +5,9 @@
     <title>Sign In</title>
     <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css" />
     <link rel="stylesheet" type="text/css" href="/CSS/style.css" />
-    <link rel="stylesheet" type="text/css" href="CSS/loginStyle.css">
+    <link rel="stylesheet" type="text/css" href="CSS/loginStyle.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/assets/images/favicon-32x32.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/assets/images/favicon-32x32.png" type="image/x-icon" />
     <!-- Boxicons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
     <link rel="stylesheet" href="./CSS/fontello.css" />
@@ -18,11 +18,11 @@
     <!-- HEADER -->
     <header class="header" id="header">
         <nav class="nav container">
-            <a href="indexasdfas.html" class="nav__logo"> Adventure </a>
+            <a href="index.html" class="nav__logo"> Adventure </a>
             <div class="nav__menu" id="nav-menu">
                 <ul class="nav__list">
                     <li class="nav__item">
-                        <a href="indexasdfas.html" class="nav__link">Home</a>
+                        <a href="index.html" class="nav__link">Home</a>
                     </li>
                     <li class="nav__item">
                         <a href="#" class="nav__link">Activities</a>
@@ -31,7 +31,7 @@
                         <a href="#" class="nav__link nav__about">About Us</a>
                     </li>
                     <li class="nav__item">
-                        <a href="register.php" class="nav__link nav__register">Sign In</a>
+                        <a href="register.html" class="nav__link nav__register">Sign In</a>
                     </li>
                 </ul>
 
@@ -45,59 +45,64 @@
             </div>
         </nav>
     </header>
+
     <div class="center">
-<<<<<<< HEAD
-        <!--      <img class="login-bg" src="assets/images/test1.webp">-->
         <div class="card" id="card">
             <form action="#" class="front">
                 <h1>Sign in</h1>
-                <div class="group"><i class="icon-mail-alt">
-                    </i><input class="email" type="email" placeholder="Email" required>
+
+                <div class="group">
+                    <i class="icon-mail-alt"></i>
+                    <input class="email1" type="email" placeholder="Email" />
                 </div>
-                <div class="group"><i class="icon-lock"></i>
-                    <input class="password" type="password" placeholder="Password" required>
+                <div class="group">
+                    <i class="icon-lock"></i>
+                    <input class="password1" type="password" placeholder="Password" />
                 </div>
-                <input type="submit" id="btn" value="Login">
-=======
-<!--      <img class="login-bg" src="assets/images/test1.webp">-->
-      <div class="card" id="card">
-        <form action="/login" method="POST" class="front">
-          <h1>Sign in</h1>
-          <div class="group"><i class="icon-mail-alt"></i><input class="email"  name="email" type="email" placeholder="Email"></div>
-          <div class="group"><i class="icon-lock"></i><input class="password" name="password" type="password" placeholder="Password"></div>
-          <input type="submit" id="btn" value="Login">
->>>>>>> f3a49bd0c42fb45a80c23fbc3ac4ee44eda9f243
+
+                <input type="submit" value="Login" />
 
                 <h2 id="register">Register</h2>
             </form>
 
-            <form action="/register" method="POST" class="back">
+            <form action="#" class="back" id="form">
                 <h1>Register</h1>
-                <div class="group"><i class="icon-user"></i>
-                    <input class="name" name="name" type="text" placeholder="Name" name="username" required>
+
+                <div class="group" id="user-form">
+                    <i class="icon-user"></i>
+                    <input id="user" type="text" placeholder="Name" oninput="userValidation()" />
+                    <i class="fas fa-check-circle move-circle"></i>
+                    <i class="fas fa-exclamation-circle move-circle"></i>
                 </div>
-                <div class=" group"><i class="icon-mail-alt"></i>
-                    <input class="email" name="email" type="email" placeholder="Email" name="email" required>
+                <div class="group" id="email-form">
+                    <i class="icon-mail-alt"></i>
+                    <input id="email" type="email" placeholder="Email" oninput="emailValidation()" />
+                    <i class="fas fa-check-circle move-circle"></i>
+                    <i class="fas fa-exclamation-circle move-circle"></i>
                 </div>
-                <div class="group"><i class="icon-lock"></i>
-                    <input class="password" name="password" type="password" placeholder="Password" name="password"
-                        required>
+                <div class="group" id="pass-form">
+                    <i class="icon-lock"></i>
+                    <input class="password2" id="pass" type="password" placeholder="Password"
+                        oninput="passwordValidation()" />
+                    <i class="fas fa-check-circle move-circle"></i>
+                    <i class="fas fa-exclamation-circle move-circle"></i>
                 </div>
-                <div class="group"><i class="icon-lock"></i>
-                    <input class="password" name="confirm_password" type="password" placeholder="Confirm password"
-                        name="cpassword" required>
+                <div class="group" id="confirm-pass-form">
+                    <i class="icon-lock"></i>
+                    <input class="password3" id="confirm-pass" type="password" placeholder="Confirm password"
+                        oninput="confirmPassValidation()" />
+                    <i class="fas fa-check-circle move-circle"></i>
+                    <i class="fas fa-exclamation-circle move-circle"></i>
                 </div>
-                <input type="submit" id="btnR" value="Register">
+
+                <!-- <button id="btn" type="submit">Register</button> -->
+                <input type="submit" id="btnR" value="Register" />
 
                 <h2 id="Login">Login</h2>
             </form>
-
         </div>
     </div>
 
-
-    <script type="text/javascript" src="event.js"></script>
-    </div>
     <script src="/JS/event.js"></script>
     <script src="/JS/main.js"></script>
 </body>
